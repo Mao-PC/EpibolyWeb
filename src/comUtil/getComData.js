9323db37448c03ed25d408ee0ca7fac6a305a97e
maopch <maopch@yonyou.com>
2019-11-20 00:31:06 +0800

    字典tag

--- src/comUtil/getComData.js
+++ src/comUtil/getComData.js
@@ -4,7 +4,7 @@ import { Select, Tree, Icon, Modal } from 'antd';
 
 const { Option } = Select;
 const { TreeNode } = Tree;
-const { confirm } = Modal
+const { confirm } = Modal;
 
 const getDept = function getDept(data) {
     axios({
@@ -29,69 +29,85 @@ const getDept = function getDept(data) {
 /**
  *  初始化树
  */
-const getTreeNodes = function (data, addable = false) {
-    axios({
-        url: 'data/treeData.json',
-        data,
-        responseType: 'json'
-    })
-        .then(res => {
-            this.setState({
-                areaTree: getSubNode.call(this, res.data, addable)
-            });
+const getTreeNodes = function(params, url, addable = false, iconEvents) {
+    axios
+        .get(url ? url : 'data/treeData.json', {
+            params,
+            responseType: 'json'
         })
-        .catch(e => console.log(e));
+        .then(res => {
+            console.log(res.data);
+            if (res.data) {
+                this.setState({
+                    areaTree: getSubNode.call(this, res.data, addable, iconEvents)
+                });
+            } else {
+                this.setState({ areaTree: [] });
+            }
+        })
+        .catch(e => {
+            console.log(e);
+            this.setState({ areaTree: [] });
+        });
 };
 
-function getSubNode(data, addable = false) {
+function getSubNode(data, addable = false, iconEvents) {
     if (data) {
         return data.map(element => {
             let flag = element.children && element.children.length;
             return (
-                <TreeNode title={
-                    <span>
-                        {element.name}
-                        {addable && <Icon
-                            style={{ paddingLeft: 10 }}
-                            type={flag ? 'plus-square' : 'minus-square'}
-                            onClick={iconClick.bind(this, flag)}
-                        />}
-                    </span>
-                }
-                    key={element.id}>
-                    {getSubNode(element.children, addable)}
+                <TreeNode
+                    title={
+                        <span>
+                            {`${element.codeName} - ${element.codeNo}`}
+                            {addable && (
+                                <Icon
+                                    style={{ paddingLeft: 10 }}
+                                    type={'plus-square'}
+                                    onClick={iconClick.bind(this, true, element, iconEvents)}
+                                />
+                            )}
+                            {addable && !Boolean(flag) && (
+                                <Icon
+                                    style={{ paddingLeft: 10 }}
+                                    type={'minus-square'}
+                                    onClick={iconClick.bind(this, false, element, iconEvents)}
+                                />
+                            )}
+                        </span>
+                    }
+                    key={element.id}
+                >
+                    {getSubNode(element.children, addable, iconEvents)}
                 </TreeNode>
             );
         });
     }
 }
 
-function iconClick(flag) {
+function iconClick(flag, data, iconEvents) {
+    this.setState({ cNode: data });
     if (flag) {
         // 增加
-        this.setState({ addOrgModalFlag: true });
+        this.setState({ addTreeModalFlag: true });
     } else {
         // 删除
-        confirm({
-            title: '确定要删除该结构吗 ?',
+        confirm.call(this, {
+            title: '确定要删除该数据吗 ?',
             // content: 'Some descriptions',
             okText: '确认',
             okType: 'danger',
             cancelText: '取消',
-            onOk() {
-                console.log('OK');
-            },
-            onCancel() {
-                console.log('Cancel');
-            }
+            onOk: () => iconEvents.okEvent.call(this),
+            onCancel: () => iconEvents.cancelEvent.call(this)
         });
     }
-};
+}
 
 /**
  *  获取表格数据
  */
-const getTableData = function (url, data) {
+const getTableData = function(url, data) {
     axios({
         url: url ? url : 'data/tableData.json',
         data,
--- src/hcc/sm/DataDic.js
+++ src/hcc/sm/DataDic.js
@@ -1,88 +1,77 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import { Tag, Input, Icon, Modal, Tree, notification } from 'antd';
 import { TweenOneGroup } from 'rc-tween-one';
 
-import { getTreeNodes } from '../../comUtil'
-import axios from "axios";
-
-const { confirm } = Modal;
+import { getTreeNodes } from '../../comUtil';
+import axios from 'axios';
 
 export default class EditableTagGroup extends Component {
-
-    newName = ''
-    newCode = ''
+    newName = '';
+    newCode = '';
 
     state = {
         inputVisible: false,
         dicModalFlag: false,
-        addOrgModalFlag: false,
-        addDicModalFlag: false,
+        addTreeModalFlag: false,
         dicTitle: '',
         areaTree: [],
-        tagChild: []
+        tagChild: [],
+        // 选中的字典
+        cNode: {},
+        // 选中字典的根
+        cRootNode: {}
     };
 
     componentDidMount() {
         // 请求所有的字典
         axios.get('/dic/listDics').then(req => {
-            console.log(req.data)
+            console.log(req.data);
             this.setState({
                 tagChild: req.data.map(tag => {
-                    const tagElem = <Tag
-                        closable
-                        onClose={e => {
-                            e.preventDefault();
-                            e.stopPropagation();
-                            this.handleClose(tag.codeName);
-                        }}
-                        onClick={() => {
-                            getTreeNodes.call(this, null, true)
-                            this.setState({ dicModalFlag: true, })
-                        }}
-                    >
-                        {tag.codeName}
-                    </Tag>
+                    const tagElem = (
+                        <Tag
+                            onClick={() => {
+                                getTreeNodes.call(this, { code: tag.codeNo }, '/dic/listDicTree', true, {
+                                    okEvent: this.okEvent,
+                                    cancelEvent: this.cancelEvent
+                                });
+                                this.setState({ dicModalFlag: true, cRootNode: tag });
+                            }}
+                        >
+                            {tag.codeName}
+                        </Tag>
+                    );
                     return (
                         <span key={tag.id} style={{ display: 'inline-block' }}>
                             {tagElem}
                         </span>
                     );
                 })
-            })
-        })
+            });
+        });
     }
 
-    handleClose = removedTag => {
-        confirm({
-            title: '确定要删除该数据吗 ?',
-            // content: 'Some descriptions',
-            okText: '确认',
-            okType: 'danger',
-            cancelText: '取消',
-            onOk: () => {
-                const tags = this.state.tags.filter(tag => tag !== removedTag);
-                console.log(tags);
-                this.setState({ tags });
-            },
-            onCancel() {
-                console.log('Cancel');
-            }
-        })
-
+    okEvent = () => {
+        axios
+            .delete('/dic/delete', { params: { id: this.state.cNode.id } })
+            .then(() => {
+                notification.success.call(this, { message: '删除成功' });
+                getTreeNodes.call(this, { code: this.state.cRootNode.codeNo }, '/dic/listDicTree', true, {
+                    okEvent: this.okEvent.bind(this),
+                    cancelEvent: this.cancelEvent.bind(this)
+                });
+            })
+            .catch(() => {
+                notification.error({ message: '删除失败' });
+            });
     };
-
-    showInput = () => {
-        this.setState({ inputVisible: true }, () => this.input.focus());
-    };
-
-
-    saveInputRef = input => (this.input = input);
+    cancelEvent = () => console.log(1111);
 
     render() {
-        const { dicModalFlag, areaTree, addOrgModalFlag, tagChild, addDicModalFlag } = this.state;
+        const { dicModalFlag, areaTree, tagChild, addTreeModalFlag, cNode } = this.state;
         return (
             <div>
-                <div className="data-tag" >
+                <div className="data-tag">
                     <TweenOneGroup
                         enter={{
                             scale: 0.8,
@@ -91,7 +80,7 @@ export default class EditableTagGroup extends Component {
                             duration: 100,
                             onComplete: e => {
                                 e.target.style = '';
-                            },
+                            }
                         }}
                         leave={{ opacity: 0, width: 0, scale: 0, duration: 200 }}
                         appear={false}
@@ -100,66 +89,63 @@ export default class EditableTagGroup extends Component {
                     </TweenOneGroup>
                 </div>
 
-                <Tag onClick={() => this.setState({ addDicModalFlag: true })} style={{ background: '#fff', borderStyle: 'dashed', marginLeft: 20 }}>
+                <Tag
+                    onClick={() => this.setState({ addTreeModalFlag: true, cNode: {} })}
+                    style={{ background: '#fff', borderStyle: 'dashed', marginLeft: 20 }}
+                >
                     <Icon type="plus" /> New Tag
-          </Tag>
+                </Tag>
                 <Modal
-                    style={{ maxHeight: "90%" }}
-                    title={'fwdfd'}
+                    style={{ maxHeight: '90%' }}
+                    title={'字典数据'}
                     visible={dicModalFlag}
                     okText="确定"
                     cancelText="取消"
                     onOk={this.handleOk}
-                    onCancel={() => this.setState({ dicModalFlag: false })}
+                    onCancel={() => location.reload()}
                 >
                     <Tree selectable={false}>{areaTree}</Tree>
                 </Modal>
                 <Modal
-                    title="请输入新增的机构名"
-                    visible={addOrgModalFlag}
-                    okText="确定"
-                    cancelText="取消"
-                    onOk={e => {
-                        console.log(e)
-                        this.setState({ addOrgModalFlag: false })
-                    }}
-                    onCancel={() => this.setState({ addOrgModalFlag: false })}
-                >
-                    <Input />
-                </Modal>
-                <Modal
                     title="新增字典"
-                    visible={addDicModalFlag}
+                    visible={addTreeModalFlag}
                     okText="确定"
                     cancelText="取消"
                     onOk={() => {
-                        axios.post('/dic/save', { codeName: this.newName, codeNo: this.newCode })
+                        axios
+                            .post('/dic/save', {
+                                codeName: this.newName,
+                                codeNo: this.newCode,
+                                codeTitle: cNode.codeName,
+                                codeTitleDesc: cNode.codeNo
+                            })
                             .then(() => {
-                                this.setState({ addDicModalFlag: false })
+                                this.setState({ addTreeModalFlag: false });
                                 notification.success({ message: '新增成功' });
                                 setTimeout(() => {
-                                    location.reload()
-                                }, 2000)
-                            }).catch(() => {
-                                this.setState({ addDicModalFlag: false })
-                                this.newCode = ''
-                                this.newName = ''
-                                notification.error({ message: '新增失败' })
+                                    location.reload();
+                                }, 2000);
                             })
+                            .catch(() => {
+                                this.setState({ addTreeModalFlag: false });
+                                this.newCode = '';
+                                this.newName = '';
+                                notification.error({ message: '新增失败' });
+                            });
                     }}
                     onCancel={() => {
-                        this.newCode = ''
-                        this.newName = ''
-                        this.setState({ addDicModalFlag: false })
+                        this.newCode = '';
+                        this.newName = '';
+                        this.setState({ addTreeModalFlag: false });
                     }}
                 >
                     <div>
                         <span className="model-span">名称： </span>
-                        <Input className="model-input" onChange={(e) => this.newName = e.target.value} />
+                        <Input className="model-input" onChange={e => (this.newName = e.target.value)} />
                     </div>
                     <div>
                         <span className="model-span">编码： </span>
-                        <Input className="model-input" onChange={(e) => this.newCode = e.target.value} />
+                        <Input className="model-input" onChange={e => (this.newCode = e.target.value)} />
                     </div>
                 </Modal>
             </div>
